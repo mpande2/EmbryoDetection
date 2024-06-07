9cd3e2f5d4ddbd91ce478f35259430edbcc8b557
Gopal Srivastava <71832523+gnsrivastava@users.noreply.github.com>
2024-06-07 14:46:02 -0500

    Update RunScript.sh

--- RunScript.sh
+++ RunScript.sh
@@ -2,6 +2,7 @@
 
 for viable in $(seq 100 50 300); do
     nonviable=$(( viable * 3 / 10 ))  # Calculate 30% as an integer
+    viable=$(( viable - nonviable ))
     python3.11 GenerateSyntheticImages_MultipleTypes.py $viable $nonviable
 done
 

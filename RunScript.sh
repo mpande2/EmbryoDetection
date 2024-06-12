a903c4bccda626fba44be85c9538180b976cab65
Gopal Srivastava <71832523+gnsrivastava@users.noreply.github.com>
2024-06-12 16:36:10 -0500

    Update RunScript.sh
    Can create variable ratio of non-viable eggs in the image.

--- RunScript.sh
+++ RunScript.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-for viable in $(seq 100 50 300); do
-    nonviable=$(( viable * 3 / 10 ))  # Calculate 30% as an integer
+for ratio in $(seq 2 2 8); do # This is to change ratio of non-viable in the images. 
+for viable in $(seq 50 10 150); do
+    nonviable=$(( viable * ratio / 10 ))  # Calculate 30% as an integer
     viable=$(( viable - nonviable ))
-    python3.11 GenerateSyntheticImages_MultipleTypes.py $viable $nonviable
+		python3.11 GenerateSyntheticImages_MultipleTypes.py $viable $nonviable
+done
 done
 
